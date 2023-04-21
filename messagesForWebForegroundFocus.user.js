f6de671a4156c8c4615a6d48d990cfd6679a778b
Mike <mike@catalystcode.io>
2023-04-21 16:01:32 -0500

    fox icon url

--- messagesForWebForegroundFocus.user.js
+++ messagesForWebForegroundFocus.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Messages for Web - Foreground Focus
 // @namespace    http://yo1.dog
-// @version      1.0.0
+// @version      1.0.1
 // @description  Messages for Web: Focus the text input on window foreground.
 // @author       Mike "yo1dog" Moore
 // @homepageURL  https://github.com/yo1dog/messages-for-web-fixes#readme
-// @icon         https://github.com/yo1dog/messages-for-web-fixes/raw/master/icon.ico
+// @icon         https://github.com/yo1dog/messages-for-web-fixes/raw/master/icon.png
 // @match        https://messages.google.com/web/*
 // @grant        none
 // @run-at       document-end
@@ -13,7 +13,7 @@
 // @unwrap
 // ==/UserScript==
 
-console.log('Messages for Web - Foreground Focus v1.0.0');
+console.log('Messages for Web - Foreground Focus v1.0.1');
 window.addEventListener('focus', () => {
   setTimeout(
     () => document.querySelector('textarea.input')?.focus(),
